32d16f51fae4b5ce8fccda5b86b0a278233ccce5
Fer Aguirre <kay_op@protonmail.com>
2023-03-23 21:09:44 -0600

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Cancilleria Colombia',
+    version='0.1.0',
+    author='Fer Aguirre',
+    description='Análisis de datos de funcionarios públicos de Relaciones Exteriores en Colombia.',
+    python_requires='>=3',
+    license='MIT License',
+    packages=find_packages(),
+)
